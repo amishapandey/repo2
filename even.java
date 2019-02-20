c603116a7e83810bf65281285248068fbacd3008
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-02-20 14:14:44 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+Class Simple{
+public static void main(){
+Scanner input=new Scanner(System.in);
+System.out.println("enter the number");
+int num=input.nextInt();
+if(num%2==0)
+System.out.println("Even");
+else
+System.out.println("Odd");
+}
+}
