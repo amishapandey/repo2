911355edad2aae8465c37019dfb7c6740526305e
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-02-20 15:29:53 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+Class Simple{
+public static void main(String args[]){
+Scanner input=new Scanner(System.in)
+String n=input.next();
+if(n=="a")
+System.out.println("Vowel");
+else if(n=="e")
+System.out.println("Vowel");
+else if(n=="i")
+System.out.println("Vowel");
+else if(n=="o")
+System.out.println("Vowel");
+else if(n=="u")
+System.out.println("Vowel");
+else
+System.out.println("Consonent");
+}
+}
