127fe35d9c0b3179275cf44bbfafc508694fecdd
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-02-20 19:22:14 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+Class Simple{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int y=input.nextInt();
+if(y%4==0 && y%100!=0 || y%400==0)
+System.out.println("yes");
+}
+}
