21383f673d6d9a93dbc532be21c94d843ec1bf08
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-02-20 19:33:57 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+Class Simple{
+public static void main(String args[]){
+Scanner input=new Scanner(System.in);
+int n=input.nextInt();
+int i;
+for(i=0;i<n;i++){
+System.out.println("Hello");
+}
+}
