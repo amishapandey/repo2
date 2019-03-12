546e65bc4c1f99ca468cdccd0b7d9d750203d68f
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-03-12 08:29:20 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+  System.out.println("
+  int n=sc.nextInt();
+  int n=sc.nextInt();
+char a[]=new char[]
+if('n'>=48 && 'n'<=
